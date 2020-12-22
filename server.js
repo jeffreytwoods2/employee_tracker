49567eea1a7ac25474154c2e70d3bfe70ec22129
GnomeSaiyan <69124994+jeffreytwoods2@users.noreply.github.com>
2020-12-21 21:03:48 -0700

    Created opening inquirer prompt with promise

--- server.js
+++ server.js
@@ -15,3 +15,33 @@ connection.connect(function(err) {
 });
 
 //Create start function w/ inquirer
+function start() {
+    inquirer
+        .prompt({
+            name: 'begin',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                "View all employees",
+                "Manage employees",
+                "Remove employees",
+                "Exit"
+            ]
+        })
+        .then((answer) => {
+            switch (answer.begin) {
+                case 'View all employees':
+                    view();
+                    break;
+                case 'Manage employees':
+                    manage();
+                    break;
+                case 'Remove employees':
+                    remove();
+                    break;
+                case 'Exit':
+                    connection.end();
+            };
+            start();
+        })
+}
